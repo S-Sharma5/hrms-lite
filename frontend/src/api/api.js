67d94d0aa408d174b19bb36b596a54d94da335f5
Shubham Sharma <shubhamsharma@Shubhams-MacBook-Pro.local>
2026-03-02 04:30:28 +0530

    Connect frontend to live backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://hrms-lite-mgk7.onrender.com",
 });
 
 export const getEmployees = () => API.get("/employees");
 export const createEmployee = (data) => API.post("/employees", data);
 export const deleteEmployee = (id) => API.delete(`/employees/${id}`);
 export const getEmployee = (id) => API.get(`/employees/${id}`);
-
 
 export const markAttendance = (data) => API.post("/attendance", data);
 export const getAttendance = (id) => API.get(`/attendance/${id}`);
